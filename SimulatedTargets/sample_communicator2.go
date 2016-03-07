c90a39121f22e03e27c9ab44c0abe05abde7b722
Pamela Sanchez <pamela.aimee.sanchez@gmail.com>
2016-03-07 14:51:04 -0800

    updated the commodities creator

--- SimulatedTargets/sample_communicator2.go
+++ SimulatedTargets/sample_communicator2.go
@@ -3,15 +3,13 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"github.com/golang/glog"
 	"github.com/vmturbo/vmturbo-go-sdk/communicator"
 	"github.com/vmturbo/vmturbo-go-sdk/sdk"
 	"net/http"
-	//	"github.com/pamelasanchezvi/vmturbo-go-sdk/communicator"
-
-	"github.com/golang/glog"
 )
 
-// Struct which hold identifying information for connecting to the VMTServer
+// Struct which holds identifying information for connecting to the VMTServer
 type ConnectionInfo struct {
 	ServerAddr         string
 	LocalAddr          string
@@ -24,33 +22,21 @@ type ConnectionInfo struct {
 	TargetIdentifier   string
 }
 
-// implementation of communicator.ServerMessageHandler interface
+// This Struct is the implementation of communicator.ServerMessageHandler interface
 type MsgHandler struct {
 	wscommunicator *communicator.WebSocketCommunicator
 	cInfo          *ConnectionInfo
 	vmtapi         *VMTApiRequestHandler
 }
 
+// This struct hold the authorization information and address for connecting to VMTurbo API
 type VMTApiRequestHandler struct {
 	vmtServerAddr      string
 	opsManagerUsername string
 	opsManagerPassword string
 }
 
-type Node struct {
-	TypeMetaUID    string
-	ObjectMetaName string
-	// Spec of type NodeSpec defines the behavior of a node.
-	NodeSpecPodCIDR       string
-	NodeSpecExternalID    string
-	NodeSpecProviderID    string
-	NodeSpecUnschedulable bool
-
-	// Status describes the current status of a Node
-	//	Status NodeStatus `json:"status,omitempty"`
-}
-
-func (node *Node) createCommoditiesBought() []*sdk.CommodityDTO {
+func CreateCommoditiesBought() []*sdk.CommodityDTO {
 	var commoditiesBought []*sdk.CommodityDTO
 	cpuUsed := float64(10)
 	cpuCapacity := float64(100)
@@ -60,7 +46,7 @@ func (node *Node) createCommoditiesBought() []*sdk.CommodityDTO {
 	return commoditiesBought
 }
 
-func (node *Node) createCommoditiesSold() []*sdk.CommodityDTO {
+func CreateCommoditiesSold() []*sdk.CommodityDTO {
 	var commoditiesSold []*sdk.CommodityDTO
 	cpuUsed := float64(0)
 	cpuCapacity := float64(1000)
@@ -74,29 +60,18 @@ func (nodeProbe *NodeProbe) generateReconcilationMetaData() *sdk.EntityDTO_Repla
 	replacementEntityMetaDataBuilder := sdk.NewReplacementEntityMetaDataBuilder()
 	replacementEntityMetaDataBuilder.Matching("IP")
 	replacementEntityMetaDataBuilder.PatchSelling(sdk.CommodityDTO_CPU)
-	/*
-		replacementEntityMetaDataBuilder.PatchSelling(sdk.CommodityDTO_MEM_ALLOCATION)
-		replacementEntityMetaDataBuilder.PatchSelling(sdk.CommodityDTO_VCPU)
-		replacementEntityMetaDataBuilder.PatchSelling(sdk.CommodityDTO_VMEM)
-		replacementEntityMetaDataBuilder.PatchSelling(sdk.CommodityDTO_APPLICATION)
-	*/
 	metaData := replacementEntityMetaDataBuilder.Build()
 	return metaData
 }
 
 func (nodeProbe *NodeProbe) buildPMEntityDTO(PM_id, displayName string, commoditiesSold []*sdk.CommodityDTO) *sdk.EntityDTO {
 	cpuUsed := float64(0)
-	//	memUsed := float64(0)
-	//	nodeMemCapacity := float64(1000)
 	nodeCpuCapacity := float64(1000)
 	entityDTOBuilder := sdk.NewEntityDTOBuilder(sdk.EntityDTO_PHYSICAL_MACHINE, PM_id)
 	entityDTOBuilder.DisplayName(displayName)
 
 	curcomm := commoditiesSold[0]
 	entityDTOBuilder = entityDTOBuilder.Sells(*curcomm.CommodityType, *curcomm.Key).Capacity(float64(nodeCpuCapacity)).Used(cpuUsed)
-	//	entityDTOBuilder = entityDTOBuilder.SetProperty("IP", "172.16.162.133")
-	//	metaData := nodeProbe.generateReconcilationMetaData()
-	//	entityDTOBuilder = entityDTOBuilder.ReplacedBy(metaData)
 	entityDTO := entityDTOBuilder.Create()
 	return entityDTO
 }
@@ -107,9 +82,6 @@ func (nodeProbe *NodeProbe) buildVMEntityDTO(VM_id, displayName, provider string
 
 	entityDTOBuilder = entityDTOBuilder.SetProvider(sdk.EntityDTO_PHYSICAL_MACHINE, provider)
 	entityDTOBuilder.BuysCommodities(commoditiesbought)
-	//ipAddress := "172.16.162.133" // ask Dongyi, getIPForStitching from pkg/vmturbo/vmt/probe/node_probe.go
-	// not using nodeProbe.generateReconcilationMetaData()
-	//Make this VM buy from a given PM , TODO check if this is the entity.Name for the PM
 	entityDTO := entityDTOBuilder.Create()
 
 	return entityDTO
@@ -117,13 +89,10 @@ func (nodeProbe *NodeProbe) buildVMEntityDTO(VM_id, displayName, provider string
 
 type NodeProbe struct {
 	// nodesGetter func
-	NodeArray []*Node // pkg.api.Node ?
+	NodeArray []*Node
 }
 
 type KubernetesProbe struct {
-	//RestClient  KubeClient *client.Client
-	//	GetNodes() returns a  []*api.Node made from  *api.NodeList .Items[] using label field  ,
-	//	Items is a []Node
 	nodeProbe *NodeProbe
 }
 
@@ -136,32 +105,30 @@ func (kProbe *KubernetesProbe) getNodeEntityDTOs() []*sdk.EntityDTO {
 	nodearr := kProbe.getNodeProbe().NodeArray
 	/* if this was a real master and had >1 node then it would loop through nodearr type []*Node
 	   for now we just harcode to the first Node*/
-	//	nodeID := nodearr[0].TypeMetaUID
-	//	dispName := nodearr[0].ObjectMetaName
 	// we call createCommoditySold to get []*sdk.CommodityDTO
 	// for now commoditiesDTOSold and bought are array of size 1, TODO: modifify createCommodities.. and buildPM for array
-	commoditiesDTOsold := nodearr[0].createCommoditiesSold()
-	commoditiesDTObought := nodearr[0].createCommoditiesBought()
+	commoditiesDTOsold := CreateCommoditiesSold()
+	commoditiesDTObought := CreateCommoditiesBought()
 	// create PM EntityDTO
-	newPMEntityDTO := kProbe.getNodeProbe().buildPMEntityDTO("PM_seller", "PAM_PM_seller", commoditiesDTOsold)
-	newEntityDTO := kProbe.getNodeProbe().buildVMEntityDTO("VM_buyer", "PAM_VM_buyer", "PM_seller", commoditiesDTObought)
+	newPMEntityDTO1 := kProbe.getNodeProbe().buildPMEntityDTO("PM_seller1", "PAM_PM_seller1", commoditiesDTOsold)
+	newVMEntityDTO1A := kProbe.getNodeProbe().buildVMEntityDTO("VM_buyer1A", "PAM_VM_buyer1A", "PM_seller1", commoditiesDTObought)
+	newVMEntityDTO1B := kProbe.getNodeProbe().buildVMEntityDTO("VM_buyer1B", "PAM_VM_buyer1B", "PM_seller1", commoditiesDTObought)
+
+	newPMEntityDTO2 := kProbe.getNodeProbe().buildPMEntityDTO("PM_seller2", "PAM_PM_seller2", commoditiesDTOsold)
+	newVMEntityDTO2A := kProbe.getNodeProbe().buildVMEntityDTO("VM_buyer2A", "PAM_VM_buyer2A", "PM_seller2", commoditiesDTObought)
+	newVMEntityDTO2B := kProbe.getNodeProbe().buildVMEntityDTO("VM_buyer2B", "PAM_VM_buyer2B", "PM_seller2", commoditiesDTObought)
+	newPMEntityDTO3 := kProbe.getNodeProbe().buildPMEntityDTO("PM_seller3", "PAM_PM_seller3", commoditiesDTOsold)
+
 	var entityDTOarray []*sdk.EntityDTO
-	entityDTOarray = append(entityDTOarray, newPMEntityDTO)
-	entityDTOarray = append(entityDTOarray, newEntityDTO)
+	entityDTOarray = append(entityDTOarray, newPMEntityDTO1)
+	entityDTOarray = append(entityDTOarray, newPMEntityDTO2)
+	entityDTOarray = append(entityDTOarray, newVMEntityDTO1A)
+	entityDTOarray = append(entityDTOarray, newVMEntityDTO1B)
+	entityDTOarray = append(entityDTOarray, newVMEntityDTO2A)
+	entityDTOarray = append(entityDTOarray, newVMEntityDTO2B)
+	entityDTOarray = append(entityDTOarray, newPMEntityDTO3)
 	return entityDTOarray
 }
-
-/*
-func (vmtapi *VMTApiRequestHandler) parseResponse(resp *http.Response) (string, error) {
-	if resp == nil {
-		return "", fmt.Errorf("response passed as argument is nil")
-	}
-	content, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	return string(content), nil
-}*/
 
 func (vmtapi *VMTApiRequestHandler) vmtApiPost(postPath, requestStr string) (*http.Response, error) {
 	fullUrl := "http://" + vmtapi.vmtServerAddr + "/vmturbo/api" + postPath + requestStr
@@ -174,16 +141,12 @@ func (vmtapi *VMTApiRequestHandler) vmtApiPost(postPath, requestStr string) (*ht
 		fmt.Println("Log: error getting response")
 		return nil, err
 	}
-	//responseContent, _ := vmtapi.parsePostResponse(resp)
 	defer response.Body.Close()
 	return response, nil
 }
 
 func (h *MsgHandler) AddTarget() {
 
-	//vmtServer := h.wscommunicator.VmtServerAddress
-	// call vmtREST api
-	// h.cInfo.Type h.cInfo.Name h.cInfo.Username h.cInfo.Password , h.cInfo.TargetIdentifier
 	var requestDataB bytes.Buffer
 	requestDataB.WriteString("?type=")
 	requestDataB.WriteString(h.cInfo.Type)
@@ -206,7 +169,6 @@ func (h *MsgHandler) AddTarget() {
 	}
 	fmt.Println("Printing AddTarget postReply:")
 	fmt.Println(postReply)
-	//	postReplyMessage, err := vMTApiRequestHandler.parseResponse(postReply)
 
 	if postReply.Status != "200 OK" {
 		fmt.Println(" postReplyMessage error")
@@ -219,7 +181,6 @@ func (h *MsgHandler) Validate(serverMsg *communicator.MediationServerMessage) {
 	// messageID is a int32 , if nil then 0
 	messageID := serverMsg.GetMessageID()
 	validationResponse := new(communicator.ValidationResponse)
-	// add something in validation response fields?? TODO
 
 	// creates a ClientMessageBuilder and sets ClientMessageBuilder.clientMessage.MessageID = messageID
 	// sets clientMessage.ValidationResponse = validationResponse
@@ -227,8 +188,6 @@ func (h *MsgHandler) Validate(serverMsg *communicator.MediationServerMessage) {
 	clientMsg := communicator.NewClientMessageBuilder(messageID).SetValidationResponse(validationResponse).Create()
 	h.wscommunicator.SendClientMessage(clientMsg)
 	glog.Infof("The client msg sent out is %++v", clientMsg)
-	// discover TODO
-	//  handler.meta.NameOrAddress passed to discoverTarget
 	var requestDataB bytes.Buffer
 	requestDataB.WriteString("?type=")
 	requestDataB.WriteString(h.cInfo.Type)
@@ -260,15 +219,12 @@ func (h *MsgHandler) Validate(serverMsg *communicator.MediationServerMessage) {
 	return
 }
 func (h *MsgHandler) DiscoverTopology(serverMsg *communicator.MediationServerMessage) {
-	// TODO
 	fmt.Println("DiscoverTopology called")
 
 	messageID := serverMsg.GetMessageID()
 	newNode := &Node{
-		TypeMetaUID:    "pamelatestNode",
-		ObjectMetaName: "pamelatestNode_MetaName",
-		// add more fields for this Node TODO
-
+	//TypeMetaUID:    "pamelatestNode",
+	//ObjectMetaName: "pamelatestNode_MetaName",
 	}
 	// make new NodeArray
 	var newNodeArray []*Node
@@ -287,14 +243,12 @@ func (h *MsgHandler) DiscoverTopology(serverMsg *communicator.MediationServerMes
 	}
 	clientMsg := communicator.NewClientMessageBuilder(messageID).SetDiscoveryResponse(discoveryResponse).Create()
 	h.wscommunicator.SendClientMessage(clientMsg)
-	// TODO h.DiscoverTarget()
 	glog.Infof("The client msg sent out is %++v", clientMsg)
 	fmt.Println(clientMsg)
 	fmt.Println("done with discover")
 	return
 }
 func (h *MsgHandler) HandleAction(serverMsg *communicator.MediationServerMessage) {
-	// TODO
 	fmt.Println("HandleAction called")
 	return
 }
@@ -328,63 +282,21 @@ func (h *MsgHandler) CreateContainerInfo(localaddr string) *communicator.Contain
 * SupplyChain definition
  */
 func createSupplyChain() []*sdk.TemplateDTO {
-	//	fakestr := "fake"
-	emptystr := "aaaa"
-	/*
-		cpuAllocationType := sdk.CommodityDTO_CPU_ALLOCATION
-		cpuAllocationTemplateComm := &sdk.TemplateCommodity{
-			Key:           &fakestr,
-			CommodityType: &cpuAllocationType,
-		}
-		memAllocationType := sdk.CommodityDTO_MEM_ALLOCATION
-		memAllocationTemplateComm := &sdk.TemplateCommodity{
-			Key:           &fakestr,
-			CommodityType: &memAllocationType,
-		}*/
+	optionalKey := "commodity_key"
 	vmsupplyChainNodeBuilder := sdk.NewSupplyChainNodeBuilder()
 	vmsupplyChainNodeBuilder = vmsupplyChainNodeBuilder.Entity(sdk.EntityDTO_VIRTUAL_MACHINE)
-	/*Selling(sdk.CommodityDTO_CPU_ALLOCATION, fakestr).Selling(sdk.CommodityDTO_MEM_ALLOCATION, fakestr).Selling(sdk.CommodityDTO_VCPU, fakestr).Selling(sdk.CommodityDTO_CPU, fakestr).Selling(sdk.CommodityDTO_VMEM, fakestr).Selling(sdk.CommodityDTO_APPLICATION, fakestr)
-	 */
 	cpuType := sdk.CommodityDTO_CPU
 	cpuTemplateComm := &sdk.TemplateCommodity{
-		Key:           &emptystr,
+		Key:           &optionalKey,
 		CommodityType: &cpuType,
 	}
-	/*
-		memType := sdk.CommodityDTO_MEM
-		memTemplateComm := &sdk.TemplateCommodity{
-			Key:           &emptystr,
-			CommodityType: &memType,
-		}*/
-	/*
-		vCpuType := sdk.CommodityDTO_VCPU
-		vmVCpu := &sdk.TemplateCommodity{
-			Key:           &fakestr,
-			CommodityType: &vCpuType,
-		}
-		vMemType := sdk.CommodityDTO_VMEM
-		vmVMem := &sdk.TemplateCommodity{
-			Key:           &fakestr,
-			CommodityType: &vMemType,
-		}*/
 	vmsupplyChainNodeBuilder = vmsupplyChainNodeBuilder.Provider(sdk.EntityDTO_PHYSICAL_MACHINE, sdk.Provider_HOSTING).Buys(*cpuTemplateComm)
 	pmSupplyChainNodeBuilder := sdk.NewSupplyChainNodeBuilder()
-	pmSupplyChainNodeBuilder = pmSupplyChainNodeBuilder.Entity(sdk.EntityDTO_PHYSICAL_MACHINE).Selling(sdk.CommodityDTO_CPU, emptystr)
-	/*	pm := sdk.EntityDTO_PHYSICAL_MACHINE
-		attr := ""
-		externalEntityLink_SEPD := &sdk.ExternalEntityLink_ServerEntityPropDef{
-			Entity:    &pm,
-			Attribute: &attr,
-		}
-		pmVMExtLinkBuilder := sdk.NewExternalEntityLinkBuilder()
-		pmVMExtLinkBuilder = pmVMExtLinkBuilder.Link(sdk.EntityDTO_VIRTUAL_MACHINE, sdk.EntityDTO_PHYSICAL_MACHINE, sdk.Provider_HOSTING).Commodity(vCpuType, true).Commodity(vMemType, true).Commodity(cpuAllocationType, true).Commodity(memAllocationType, true).ProbeEntityPropertyDef(sdk.SUPPLYCHAIN_CONSTANT_IP_ADDRESS, "172.16.162.135").ExternalEntityPropertyDef(externalEntityLink_SEPD)
-		pmVMExternalLink := pmVMExtLinkBuilder.Build()
-	*/
+	pmSupplyChainNodeBuilder = pmSupplyChainNodeBuilder.Entity(sdk.EntityDTO_PHYSICAL_MACHINE).Selling(sdk.CommodityDTO_CPU, optionalKey)
 	/*SupplyChain building*/
 	supplyChainBuilder := sdk.NewSupplyChainBuilder()
 	supplyChainBuilder.Top(vmsupplyChainNodeBuilder)
 	supplyChainBuilder.Entity(pmSupplyChainNodeBuilder)
-	//	supplyChainBuilder.ConnectsTo(pmVMExternalLink)
 
 	return supplyChainBuilder.Create()
 }
